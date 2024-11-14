b1d8c4f72bdc942c3b45636ead8fb275c00f0450
Sam Goldenberg <sgolden140@gmail.com>
2024-11-13 20:36:00 -0800

    changing logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
+import camera1 from "./assets/camera1.png"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+        <img src={camera1} className="logo react" alt="React logo" />
 
         <h1>Photo Reminder Application</h1>
       </header>
